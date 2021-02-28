6ca687904fac8de60663b2c615ba5d58b6ad7acb
Mindaugas Dirma <mindaugas.dirma@gmail.com>
2021-02-28 20:58:12 +0200

    header right

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,28 +1,53 @@
 import React, { useLayoutEffect } from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
 import CustomListItem from "../components/CustomListItem";
 import { Avatar } from "react-native-elements";
 import { auth, db } from "../firebase";
+import { TouchableOpacity } from "react-native";
+import { SimpleLineIcons, AntDesign } from "@expo/vector-icons";
 
 const HomeScreen = ({ navigation }) => {
+  const signOutUser = () => {
+    auth.signOut().then(() => {
+      navigation.replace("Login");
+    });
+  };
+
   useLayoutEffect(() => {
     navigation.setOptions({
       title: "Signal",
       headerStyle: { backgroundColor: "#fff" },
       headerTitleStyle: { color: "black" },
       headerTintColor: "black",
-      headerLeft: () => {
+      headerLeft: () => (
         <View style={{ marginLeft: 20 }}>
-          <Avatar
-            rounded
-            source={{
-              uri:
-                "https://toppng.com/uploads/preview/roger-berry-avatar-placeholder-11562991561rbrfzlng6h.png",
-            }}
-          />
-        </View>;
-      },
+          <TouchableOpacity onPress={signOutUser} activeOpacity={0.5}>
+            <Avatar rounded source={{ uri: auth?.currentUser?.photoURL }} />
+          </TouchableOpacity>
+        </View>
+      ),
+      headerRight: () => (
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            width: 80,
+            marginRight: 20,
+          }}
+        >
+          <TouchableOpacity activeOpacity={0.5}>
+            <AntDesign name="camerao" size={24} color="black" />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <SimpleLineIcons
+              onPress={navigation.navigate("addChat")}
+              name="pencil"
+              size={24}
+              color="black"
+            />
+          </TouchableOpacity>
+        </View>
+      ),
     });
   }, []);
 
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -18,7 +18,11 @@ const LoginScreen = ({ navigation }) => {
     return unsubscribe;
   }, []);
 
-  const signIn = () => {};
+  const signIn = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => alert(error));
+  };
 
   return (
     <KeyboardAvoidingView behavior="padding" style={styles.containerStyle}>
@@ -45,6 +49,7 @@ const LoginScreen = ({ navigation }) => {
           type="password"
           value={password}
           onChangeText={(text) => setPassword(text)}
+          onSubmitEditing={signIn}
         />
       </View>
       <Button containerStyle={styles.button} onPress={signIn} title="Login" />
